17f5becee15d8b4040e03f25d4d628a1a453869d
yizzuide <fu837014586@163.com>
2023-11-28 23:03:08 +0800

    chore(tests): update demo

--- tests/test_runner/test_runner_example.py
+++ tests/test_runner/test_runner_example.py
@@ -4,6 +4,4 @@ from autoboot_web import WebRunner
 # uvicorn tests.test_runner.test_runner_example:app --host 127.0.0.1 --port 8001
 autoboot = AutoBoot(config=AutoBootConfig(config_dir="./tests/test_runner"))
 autoboot.apply(WebRunner(scan_controllers="tests.test_runner.controller"))
-autoboot.run()
-
-app = WebRunner.get_context()
+app = autoboot.run(lambda: WebRunner.get_context())
