4a5eb3e763d2db2ba497acc14c919c03219bd895
yizzuide <fu837014586@163.com>
2023-11-29 00:05:34 +0800

    chore(tests): update demo

--- tests/test_runner/test_runner_example.py
+++ tests/test_runner/test_runner_example.py
@@ -1,7 +1,8 @@
-from autoboot import AutoBoot, AutoBootConfig
+from autoboot import AutoBoot, AutoBootConfig, ApplicationProperties
 from autoboot_web import WebRunner
 
-# uvicorn tests.test_runner.test_runner_example:app --host 127.0.0.1 --port 8001
+# uvicorn tests.test_runner.test_runner_example:app --host 127.0.0.1 --port 8001 --env-file ./tests/test_runner/.env
 autoboot = AutoBoot(config=AutoBootConfig(config_dir="./tests/test_runner"))
 autoboot.apply(WebRunner())
 app = autoboot.run(lambda: WebRunner.get_context())
+autoboot.logger.info(ApplicationProperties.app_name())
