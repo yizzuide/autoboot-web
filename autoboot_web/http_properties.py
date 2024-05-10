4f640e53b60c8ec69b68fc7146f91ce73e1a4ac1
yizzuide <fu837014586@163.com>
2024-05-11 04:01:25 +0800

    refactor(env): get config property used  instead of the

--- autoboot_web/http_properties.py
+++ autoboot_web/http_properties.py
@@ -1,46 +1,38 @@
 
 import uuid
-from autoboot.annotation import value_component
+from autoboot.annotation import static_property
 
 class HttpProperties:
   
-  @value_component("autoboot.web.http.gzip.enable")
-  @staticmethod
+  @static_property("autoboot.web.http.gzip.enable")
   def gzip_enable() -> bool:
     return False
 
-  @value_component("autoboot.web.http.gzip.minimum_size")
-  @staticmethod
+  @static_property("autoboot.web.http.gzip.minimum_size")
   def minimum_size() -> int:
     return 1000
 
-  @value_component("autoboot.web.http.session.enable")
-  @staticmethod
+  @static_property("autoboot.web.http.session.enable")
   def session_enable() -> bool:
     return False
   
-  @value_component("autoboot.web.http.session.secret")
-  @staticmethod
+  @static_property("autoboot.web.http.session.secret")
   def session_secret_key() -> str:
     return str(uuid.uuid1())
 
-  @value_component("autoboot.web.http.session.cookie_name")
-  @staticmethod
+  @static_property("autoboot.web.http.session.cookie_name")
   def session_cookie_name() -> str:
     return "session_id"
   
-  @value_component("autoboot.web.http.session.max_age")
-  @staticmethod
+  @static_property("autoboot.web.http.session.max_age")
   def session_max_age() -> int:
     return 3600
 
-  @value_component("autoboot.web.http.session.same_site")
-  @staticmethod
+  @static_property("autoboot.web.http.session.same_site")
   def session_same_site() -> str:
     return "lax"
   
-  @value_component("autoboot.web.http.session.https_only")
-  @staticmethod
+  @static_property("autoboot.web.http.session.https_only")
   def session_https_only() -> bool:
     return False
   
--- autoboot_web/web_properties.py
+++ autoboot_web/web_properties.py
@@ -1,65 +1,53 @@
 
 import uuid
-from autoboot.annotation import value_component
+from autoboot.annotation import static_property
 
 class WebProperties:
   
-  @value_component("autoboot.web.scan_controller_packages")
-  @staticmethod
+  @static_property("autoboot.web.scan_controller_packages")
   def scan_controller_packages() -> list[str]:
     pass
   
-  @value_component("autoboot.web.security.cors.cross_origin")
-  @staticmethod
+  @static_property("autoboot.web.security.cors.cross_origin")
   def cross_origin() -> bool:
     return False
 
-  @value_component("autoboot.web.security.cors.allow_origins")
-  @staticmethod
+  @static_property("autoboot.web.security.cors.allow_origins")
   def allow_origins() -> list[str]:
     return ["*"]
 
-  @value_component("autoboot.web.security.cors.allow_methods")
-  @staticmethod
+  @static_property("autoboot.web.security.cors.allow_methods")
   def allow_methods() -> list[str]:
     return ["*"]
 
-  @value_component("autoboot.web.security.cors.allow_headers")
-  @staticmethod
+  @static_property("autoboot.web.security.cors.allow_headers")
   def allow_headers() -> list[str]:
     return ["*"]
 
-  @value_component("autoboot.web.security.cors.allow_credentials")
-  @staticmethod
+  @static_property("autoboot.web.security.cors.allow_credentials")
   def allow_credentials() -> bool:
     return True
   
-  @value_component("autoboot.web.security.cors.max_age")
-  @staticmethod
+  @static_property("autoboot.web.security.cors.max_age")
   def max_age() -> int:
     return 3600
 
-  @value_component("autoboot.web.security.csrf.enabled")
-  @staticmethod
+  @static_property("autoboot.web.security.csrf.enabled")
   def csrf_enabled() -> bool:
     return False
   
-  @value_component("autoboot.web.security.csrf.secret")
-  @staticmethod
+  @static_property("autoboot.web.security.csrf.secret")
   def csrf_secret() -> str:
     return str(uuid.uuid1())
 
-  @value_component("autoboot.web.security.csrf.cookie_name")
-  @staticmethod
+  @static_property("autoboot.web.security.csrf.cookie_name")
   def csrf_cookie_name() -> str:
     return "csrf_token"
   
-  @value_component("autoboot.web.security.csrf.csrf_cookie_domain")
-  @staticmethod
+  @static_property("autoboot.web.security.csrf.csrf_cookie_domain")
   def csrf_cookie_domain() -> str:
     return None
 
-  @value_component("autoboot.web.security.csrf.header_name")
-  @staticmethod
+  @static_property("autoboot.web.security.csrf.header_name")
   def csrf_header_name() -> str:
     return "X-CSRF-TOKEN"
